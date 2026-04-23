04427faea0def86120fb49e9420b30aa0a3f7eb7
Michelle Lusiana Prasetya <M01372@banksinarmas.com>
2026-04-23 11:11:20 +0700

    update dengan menambahkan Navbar di Home

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import searchLogo from './assets/searchLogo.png'
+function Home() {
+
+    return (
+        <div className="Home">
+        </div>
+    )
+
+}
+
+export default Home
